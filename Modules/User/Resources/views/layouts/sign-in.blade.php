d6e16cf0d1338214ee1a5a94cac2ff852d86db33
m-zam <mohammadzam94@yahoo.com>
2022-08-05 04:18:47 +0430

    finished crate user sign-in page

--- Modules/User/Resources/views/layouts/sign-in.blade.php
+++ Modules/User/Resources/views/layouts/sign-in.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{url('assets/img/apple-icon.png')}}">
     <link rel="icon" type="image/png" href="{{url('assets/img/favicon.png')}}">
@@ -10,15 +10,16 @@
         {{$page_name}}
     </title>
     <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"/>
     <!-- Nucleo Icons -->
-    <link href="{{url('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
-    <link href="{{url('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/nucleo-icons.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{url('assets/css/nucleo-svg.css')}}" rel="stylesheet" type="text/css"/>
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="{{url('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/nucleo-svg.css')}}" rel="stylesheet"/>
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{url('assets/css/soft-ui-dashboard.css?v=1.0.3')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/soft-ui-dashboard.css')}}" rel="stylesheet" type="text/css"/>
+
 </head>
 
 <body class="">
@@ -26,12 +27,15 @@
     <div class="row">
         <div class="col-12">
             <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+            <nav
+                class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                 <div class="container-fluid">
                     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="#">
-                        Motarjem 4 You:  Sign in to admin dashboard
+                        Newsifier Project Task: Sign in
                     </a>
-                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                            aria-label="Toggle navigation">
               <span class="navbar-toggler-icon mt-2">
                 <span class="navbar-toggler-bar bar1"></span>
                 <span class="navbar-toggler-bar bar2"></span>
@@ -41,34 +45,16 @@
                     <div class="collapse navbar-collapse" id="navigation">
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item">
-                                {{--                                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="#">--}}
-                                {{--                                    <i class="fas fa-home opacity-6 text-dark me-1"></i>--}}
-
-                                {{--                                    Homepage--}}
-                                {{--                                </a>--}}
-                            </li>
-                            <li class="nav-item">
-                                {{--                                <a class="nav-link me-2" href="../pages/profile.html">--}}
-                                {{--                                    <i class="fa fa-user opacity-6 text-dark me-1"></i>--}}
-                                {{--                                    Profile--}}
-                                {{--                                </a>--}}
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="{{route('show.sign-up.page')}}">
+                                <a class="nav-link me-2" href="{{route('user.show.sign-up')}}">
                                     <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
                                     Sign Up
                                 </a>
                             </li>
-                            <li class="nav-item">
-                                {{--                                <a class="nav-link me-2" href="../pages/sign-in.html">--}}
-                                {{--                                    <i class="fas fa-key opacity-6 text-dark me-1"></i>--}}
-                                {{--                                    Sign In--}}
-                                {{--                                </a>--}}
-                            </li>
                         </ul>
                         <ul class="navbar-nav d-lg-block d-none">
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-sm btn-round mb-0 me-1 bg-gradient-dark">Back To Home Page</a>
+                                <a href="https://www.creative-tim.com/product/soft-ui-dashboard"
+                                   class="btn btn-sm btn-round mb-0 me-1 bg-gradient-dark">Back To Home Page</a>
                             </li>
                         </ul>
                     </div>
@@ -86,33 +72,29 @@
                     <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
                         <div class="card card-plain mt-8">
                             <div class="card-header pb-0 text-left bg-transparent">
-                                <h3 class="font-weight-bolder text-info text-gradient">Welcome To Motarjem 4 You... </h3>
-                                <p class="mb-0">Enter your email or teacher number and password to sign in</p>
+                                <h3 class="font-weight-bolder text-info text-gradient">Welcome To Newsifier Project
+                                    Task... </h3>
+                                <p class="mb-0">Enter your phone number and password to sign in</p>
                             </div>
                             <div class="card-body">
                                 @if(Session::has('error'))
                                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                         {{--                                        <span class="alert-icon"><i class="ni ni-like-2"></i></span>--}}
-                                        <span class="alert-text"><strong>Error!</strong> {{Session::get('error')}}</span>
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                        <span
+                                            class="alert-text"><strong>Error!</strong> {{Session::get('error')}}</span>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                 @endif
-                                @if(Session::has('success'))
-                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                        <span class="alert-icon"><i class="ni ni-like-2"></i></span>
-                                        <span class="alert-text"><strong>Success!</strong> {{Session::get('success')}}</span>
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                @endif
-                                @if($errors->has('email'))
+                                @if($errors->has('phone'))
 
                                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                        <span class="alert-text"><strong>Error!</strong> {{ $errors->first('email') }}</span>
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                        <span
+                                            class="alert-text"><strong>Error!</strong> {{ $errors->first('phone') }}</span>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
@@ -121,29 +103,30 @@
 
                                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                         <span class="alert-text"><strong>Error!</strong> {{ $errors->first('password') }}</span>
-                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                 @endif
-                                <label>Email Or Teacher Number</label>
+                                <label>Phone Number</label>
 
-                                <form method="POST" action="{{route('admin.signing-in')}}" role="form" >
+                                <form method="POST" action="{{route('user.signing-in')}}" role="form">
                                     @csrf
-
                                     <div class="mb-3">
-                                        <input name="email" value="{{old('email')}}" type="text" class="form-control" placeholder="Email Or Teacher Number" aria-label="Email" aria-describedby="email-addon">
+                                        <input name="phone" value="{{old('phone')}}" type="text" class="form-control"
+                                               placeholder="Phone Number" aria-label="phone"
+                                               aria-describedby="phone-addon">
                                     </div>
                                     <label>Password</label>
                                     <div class="mb-3">
-                                        <input name="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
-                                    </div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
-                                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        <input name="password" type="password" class="form-control"
+                                               placeholder="Password" aria-label="Password"
+                                               aria-describedby="password-addon">
                                     </div>
                                     <div class="text-center">
-                                        <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                                        <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in
+                                        </button>
                                         {{--                                        <input type="submit" value="Sign in" class="btn bg-gradient-info w-100 mt-4 mb-0">--}}
                                     </div>
                                 </form>
@@ -151,14 +134,17 @@
                             <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                 <p class="mb-4 text-sm mx-auto">
                                     Don't have an account?
-                                    <a href="{{route('show.sign-up.page')}}" class="text-info text-gradient font-weight-bold">Sign up</a>
+                                    <a href="{{route('user.show.sign-up')}}"
+                                       class="text-info text-gradient font-weight-bold">Sign up</a>
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-                            <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
+                            <div
+                                class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
+                                style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
                         </div>
                     </div>
                 </div>
@@ -166,78 +152,6 @@
         </div>
     </section>
 </main>
-<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-<footer class="footer py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mb-4 mx-auto text-center">
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Company
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    About Us
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Team
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Products
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Blog
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Pricing
-                </a>
-            </div>
-            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                    <span class="text-lg fab fa-dribbble"></span>
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                    <span class="text-lg fab fa-twitter"></span>
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                    <span class="text-lg fab fa-instagram"></span>
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                    <span class="text-lg fab fa-pinterest"></span>
-                </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                    <span class="text-lg fab fa-github"></span>
-                </a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-8 mx-auto text-center mt-1">
-                <p class="mb-0 text-secondary">
-                    Copyright © <script>
-                        document.write(new Date().getFullYear())
-                    </script> Soft by Creative Tim.
-                </p>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-<!--   Core JS Files   -->
-<script src="{{url('assets/js/core/popper.min.js')}}"></script>
-<script src="{{url('assets/js/core/bootstrap.min.js')}}"></script>
-<script src="{{url('assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
-<script src="{{url('assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
-<script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-        var options = {
-            damping: '0.5'
-        }
-        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-</script>
-<!-- Github buttons -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="{{url('assets/js/soft-ui-dashboard.min.js?v=1.0.3')}}"></script>
 </body>
 
 </html>
