f289dd7cec2f4a015035835d9d54084c6f930968
m-zam <mohammadzam94@yahoo.com>
2022-08-09 00:35:54 +0430

    fix redirectTo rout

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,8 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            alert()->toast('To post an article you must login first', 'error');
+            return route('user.show.sign-in');
         }
     }
 }
