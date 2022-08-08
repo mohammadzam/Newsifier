a492f9a2f8d743132c3ef19a2356013f8abf4d65
m-zam <mohammadzam94@yahoo.com>
2022-08-09 02:27:08 +0430

    delete unused routes

--- Modules/Article/Routes/web.php
+++ Modules/Article/Routes/web.php
@@ -12,33 +12,8 @@
 */
 
 use Illuminate\Support\Facades\Route;
-use Modules\Article\Http\Controllers\ArticleController;
-
-
-Route::post('test1', function () {
-    dd(json_decode(request()->article));
-
-})->name('test-save');
-Route::prefix('user/article')->group(function() {
+Route::prefix('user/article')->middleware('auth:web')->group(function() {
     Route::get('create-new-article', 'ArticleController@showCreateForm')->name('user.show.create.article.form');
-
-    Route::post('upload-test', function () {
-        return response()->json([
-            'success' => 1,
-            'file' => [
-                "url" => "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
-            ],
-        ]);
-    });
-    Route::post('fetchUrl', function () {
-        return response()->json([
-            'success' => 1,
-            'file' => [
-                "url" => "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif",
-            ],
-        ]);
-    });
-
+    Route::post('store-new-article', 'ArticleController@storeArticle')->name('user.store.new.article');
 });
 
-Route::post('get-image', [ArticleController::class, 'getData'])->name('user.test');
