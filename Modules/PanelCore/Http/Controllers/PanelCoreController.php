429c1a9faed28507a7e1f3daa349a8b677d60ce4
m-zam <mohammadzam94@yahoo.com>
2022-08-09 04:36:10 +0430

    create function to get data from database and return it to home page

--- Modules/PanelCore/Http/Controllers/PanelCoreController.php
+++ Modules/PanelCore/Http/Controllers/PanelCoreController.php
@@ -5,13 +5,17 @@ namespace Modules\PanelCore\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Article\Entities\Article;
 
 class PanelCoreController extends Controller
 {
 
     public function index()
     {
-        return view('panelcore::layouts.master');
+        $page_name =' Newsifier | Main';
+        $data = Article::withoutTrashed()->with('user')->get();
+//        dd($data);
+        return view('panelcore::layouts.master',compact('page_name','data'));
     }
 
 }
