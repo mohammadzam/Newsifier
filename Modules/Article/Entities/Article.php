fd6551e7543f2cf52655d00ae8cbf26372fc53b5
m-zam <mohammadzam94@yahoo.com>
2022-08-09 04:33:53 +0430

    add user_id into fillable

--- Modules/Article/Entities/Article.php
+++ Modules/Article/Entities/Article.php
@@ -5,6 +5,7 @@ namespace Modules\Article\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\User\Entities\User;
 
 class Article extends Model
 {
@@ -12,8 +13,12 @@ class Article extends Model
 
     protected $fillable = [
         'title',
+        'user_id',
         'image',
     ];
 
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
 }
